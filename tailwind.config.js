09f62fa54e7329e0ef66fc5ca596d3afe5735449
coder7475 <robiulhossain7475@gmail.com>
2023-10-18 11:09:56 +0600

    feat: project setup with tailwindcss and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display : "'Titillium Web', sans-serif",
+      },
+    },
+    colors: {
+      'white': '#FFF',
+      'darkBlue': "#687FEFF",
+      'blue': "#80B3FF",
+      "skyBlue": "#98E4FF",
+      "mint": "#B6FFA"
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+}
