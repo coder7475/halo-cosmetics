44646620db9c4682947bcec7fb6c899892b87d14
coder7475 <robiulhossain7475@gmail.com>
2023-10-18 14:35:07 +0600

    feat: setup homepage to route route

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+  return (
+    <div>
+      My HomePage      
+    </div>
+  );
+};
+
+export default HomePage;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from '../pages/ErrorPage';
 import MainLayout from '../layouts/MainLayout';
+import HomePage from '../components/HomePage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />
+      }
+    ]
   },
 ]);
 
