1309f731075c2665a57c1069f94786ac1f4b36cb
coder7475 <robiulhossain7475@gmail.com>
2023-10-19 11:58:16 +0600

    feat: change the brand input type

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -9,6 +9,7 @@ const AddProduct = () => {
     const type = form.get("type");
     const rating = form.get("rating");
     const description = form.get("description");
+
     console.log(productName, image, brand, price);
     console.log(type, rating, description);
   }
@@ -57,21 +58,28 @@ const AddProduct = () => {
                 required=""
               />
             </div>
-            <div className="w-full">
+            <div>
               <label
                 htmlFor="brand"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Brand
               </label>
-              <input
-                type="text"
-                name="brand"
-                id="brand"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Product brand"
-                required=""
-              />
+              <select
+                id="type"
+                name="type"
+                defaultValue={'DEFAULT'}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              >
+                <option value="DEFAULT" disabled>Select brand</option>
+                <option value="Flower Cosmetics">Flower Cosmetics</option>
+                <option value="Naturae Skincare">Naturae Skincare</option>
+                <option value="Luminance Makeup">Luminance Makeup</option>
+                <option value="Borcelle Salon">Borcelle Salon</option>
+                <option value="Nails Fashion">Nails Fashion</option>
+                <option value="Luxme Hair Extensions">Luxme Hair Extensions</option>
+                
+              </select>
             </div>
             <div className="w-full">
               <label
