2d118409d2324db471a2038a28d241689f133186
coder7475 <robiulhossain7475@gmail.com>
2023-10-20 11:44:30 +0600

    feat: add a update form with default value in update button page

--- src/pages/UpdateProduct.jsx
+++ src/pages/UpdateProduct.jsx
@@ -1,9 +1,168 @@
+import { useLoaderData, useParams } from "react-router-dom";
 
 const UpdateProduct = () => {
+  const { id } = useParams();
+  const { products } = useLoaderData();
+  // console.log(id, products);
+  const product = products.find(product => product._id === id);
+  console.log(product);
   return (
-    <div>
-      Update Product      
-    </div>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+        <h2 className="mb-10 text-darkBlue text-5xl font-bold text-gray-900 dark:text-white text-center">
+          Update the product
+        </h2>
+
+        <form 
+          className="form-control"
+        >
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Product Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                defaultValue={product.name}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type product name"
+                required
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="image"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Product Image
+              </label>
+              <input
+                type="text"
+                name="image"
+                id="image"
+                defaultValue={product.image}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Paste Image URL here"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="brand"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Brand
+              </label>
+              <select
+                id="brand"
+                name="brand"
+                defaultValue={product.brand}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                required
+              >
+                <option value="DEFAULT" disabled>Select brand</option>
+                <option value="Flower Cosmetics">Flower Cosmetics</option>
+                <option value="Naturae Skincare">Naturae Skincare</option>
+                <option value="Luminance Makeup">Luminance Makeup</option>
+                <option value="Borcelle Salon">Borcelle Salon</option>
+                <option value="Nails Fashion">Nails Fashion</option>
+                <option value="Luxme Hair Extensions">Luxme Hair Extensions</option>
+                
+              </select>
+            </div>
+            <div className="w-full">
+              <label
+                htmlFor="price"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Price
+              </label>
+              <input
+                type="number"
+                name="price"
+                id="price"
+                defaultValue={product.price}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="9999"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="type"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Type
+              </label>
+              <select
+                id="type"
+                name="type"
+                defaultValue={product.type}
+                required
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              >
+                <option disabled>Select type</option>
+                <option value="MK">Makeup </option>
+                <option value="hC">Hair Care</option>
+                <option value="SC">Skin Care</option>
+                <option value="BC">Body Care</option>
+                <option value="NC">Nail Care</option>
+              </select>
+            </div>
+            <div>
+              <label
+                htmlFor="type"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Rating
+              </label>
+              <select
+                id="rating"
+                name="rating"
+                defaultValue={product.name}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                required
+              >
+                <option disabled  >Select rating</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="description"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Description
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                rows="8"
+                defaultValue={product.description}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Your description here"
+                required
+              ></textarea>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800 btn btn-primary"
+          >
+            Update product
+          </button>
+        </form>
+      </div>
+    </section>
   );
 };
 
